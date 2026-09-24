04ab6246648b53a8f1c7219d5a9291d6b8a99cd6
Pam <44212866+licpmb@users.noreply.github.com>
2026-09-24 15:20:57 -0300

    fix: make logout immediate again

--- components/atlas/atlas-app.tsx
+++ components/atlas/atlas-app.tsx
@@ -1,14 +1,12 @@
 'use client'
 
-import { startTransition, useState } from 'react'
+import { useState } from 'react'
 import { LoginScreen } from './login-screen'
 import { AtlasShell } from './atlas-shell'
 
 export function AtlasApp() {
   const [authed, setAuthed] = useState(false)
 
-  if (!authed) {
-    return <LoginScreen onLogin={() => startTransition(() => setAuthed(true))} />
-  }
-  return <AtlasShell onLogout={() => startTransition(() => setAuthed(false))} />
+  if (!authed) return <LoginScreen onLogin={() => setAuthed(true)} />
+  return <AtlasShell onLogout={() => setAuthed(false)} />
 }
